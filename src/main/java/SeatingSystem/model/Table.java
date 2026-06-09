2e0d58c9f98fdcf696cea62bf89dd2c04ed28a98
prabindhojthapamagar <prabindhojthapamagar@gmail.com>
2026-06-09 16:59:49 +0900

    adding zone functionality through boolean for now for easy understanding

--- src/main/java/SeatingSystem/model/Table.java
+++ src/main/java/SeatingSystem/model/Table.java
@@ -5,10 +5,17 @@ public class Table {
 	private String tableId;
 	private int capacity;
 	
-	public Table( String TableId, int capacity) {
+	private boolean nearBuffet;
+	private boolean nearBar;
+	private boolean sofa;
+	
+	public Table( String TableId, int capacity, boolean nearBuffet, boolean nearBar, boolean sofa) {
 		
 		this.tableId = TableId;
 		this.capacity = capacity;
+		this.nearBuffet = nearBuffet;
+		this.nearBar = nearBar;
+		this.sofa = sofa;
 		
 	}
 	
@@ -20,7 +27,18 @@ public class Table {
 		return capacity;
 	}
 	
+	public boolean isNearBuffet() {
+		return nearBuffet;
+	}
 	
+	public boolean isNearBar() {
+		return nearBar;
+	}
+	
+	public boolean isSofa() {
+		return sofa;
+	}
+	 
 	@Override
 	public String toString() {
 		return tableId + "(" + capacity + " seats)";
